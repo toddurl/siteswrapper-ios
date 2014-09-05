f8e62a590d554edd4c07a8b1dfd272f8f91e437d
Todd Url <url@Todds-MacBook-Air.local>
2014-09-04 18:14:38 -0700

    Added Landing storyboard, view and navigation controllers

---
+++ SitesWrapper-iOS/SitesWrapper-iOS/LandingsNavigationController.h
@@ -0,0 +1,15 @@
+//
+//  LandingsNavigationController.h
+//  BitesWrapper-iOS
+//
+//  Created by Todd Url on 9/2/14.
+//  Copyright (c) 2014 Todd Url. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LandingsNavigationController : UINavigationController
+
+- (void)awakeFromNib;
+
+@end
